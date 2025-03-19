5bee998e332e633dda32dd4ea1341f7090a11c01
KauanneL <kauanne.l@escolar.ifrn.edu.br>
2025-03-19 07:45:09 -0300

    correção

--- coração.py
+++ coração.py
@@ -5,7 +5,7 @@ def hearta(k):
     return 15*math.sin(k)**3
 
 def heartb(k):
-    return 12*math.cos(k)-5\
+    return 12*math.cos(k)-5*\
         math.cos(2*k)-2*\
         math.cos(3*k)-\
         math.cos(4*k)
